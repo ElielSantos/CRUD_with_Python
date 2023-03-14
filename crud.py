405a5cf4b3b5e643f109e72d602b7cca7b1164f3
ElielSantos <35500688+ElielSantos@users.noreply.github.com>
2023-03-14 00:47:42 -0300

    Add files via upload

---
+++ crud.py
@@ -0,0 +1,54 @@
+#CREATE
+#nome_produto = "biscoito"
+#valor = 4
+#comando = f'INSERT INTO vendas (nome_produto, valor) VALUES("{nome_produto}",{valor})'
+#cursor.execute(comando)
+#conexao.commit() 
+
+#READ
+#comando =  f'SELECT * FROM banco1.vendas'
+#cursor.execute(comando)
+#resultado = cursor.fetchall() #LER O BANCO DE DADOS
+#print(resultado)
+
+#UPDATE
+#nome_produto = "halls"
+#valor = 1
+#comando = f'UPDATE vendas SET valor = {valor} WHERE nome_produto = "{nome_produto}"'
+#cursor.execute(comando)
+#conexao.commit() # editar banco de dados
+
+#DELETE
+#nome_produto = "biscoito"
+#comando = f'DELETE FROM vendas WHERE nome_produto "{nome_produto}"
+#cursor.execute(comando)
+#conexao.commit() 
+
+# comando = ''
+# cursor.execute(comando)
+# conexao.commit() # edita o banco de dados  
+# resultado = cursos.fechtall() #ler o banco de dados
+
+import mysql.connector #conexao com o MySQL
+
+conexao = mysql.connector.connect(
+    host='localhost', #your server MySQL
+    user='root',      #your name user MySQL 
+    password='your password',
+    database='banco1', #your name database create in MySQL
+)
+
+cursor = conexao.cursor() #1 abre a conexao 
+
+#CRUD #aqui abre os crud entre o #1 e o #2
+nome_produto = "halls1"
+comando = f'DELETE FROM vendas WHERE nome_produto "{nome_produto}"'
+cursor.execute(comando)
+conexao.commit() #editar banco de dados
+
+
+cursor.close() 
+conexao.close() #2 fecha a conexao 
+
+
+
